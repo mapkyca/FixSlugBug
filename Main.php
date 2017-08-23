2984815738783e3a545eeba355043ffe50c19d16
Marcus Povey <marcus@marcus-povey.co.uk>
2017-08-23 12:50:19 +0100

    Display something; not actually implemented yet

--- Main.php
+++ Main.php
@@ -15,8 +15,25 @@
 	    \Idno\Core\Idno::site()->addPageHandler('/admin/slugbug/?', 'IdnoPlugins\FixSlugBug\Pages\Admin');
         }
 	
+	public static function writeln($text = '') {
+	    echo "$text\n";
+	}
+	
 	public static function fix1864($dryrun = false) {
 	    
+	    if ($dryrun) self::writeln ("**************** DRY RUN *****************");
+	    
+	    self::writeln("Execution started at " . date('r'));
+	    self::writeln();
+	    
+	    
+	    
+	    
+	    
+	    
+	    self::writeln("Execution finished at " . date('r'));
+	    
+	    if ($dryrun) self::writeln ("**************** DRY RUN *****************");
 	}
 	
     }
